689cf1a8210149ef4f442be34c337c48a3b9113b
3ASKS037 <00001081379480sp@al.educacao.sp.gov.br>
2024-11-25 13:17:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cultura Pop',
+    'Princesa da disney q tem cabelos longos e vive em uma torre',
+    'Rapunzel'
+)
+
+criaCartao(
+    '... Pop',
+    'Nome do barco do Luffy em One Pice?',
+    'Thousand Sunny (atual) Going Merry (antigo)'
+)
+
+criaCartao(
+    'Pop',
+    'O que é o Stitch do Filme Lilo & Stitch?',
+    'Um Alien "fofo"  '
+)
+
+criaCartao(
+    'sla',
+    'Minha media tá como?',
+    'Muito Boaaaa!!!'
+)
